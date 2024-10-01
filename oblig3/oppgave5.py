c95d1b1eecb04caa49c850c7263a8155eef0b7bf
isuk <isak.olsvold@gmail.com>
2024-10-01 19:12:44 +0200

    almost done with oppgave5

--- oblig3/oppgave5.py
+++ oblig3/oppgave5.py
@@ -1,4 +1,6 @@
 
+programRunning = True
+
 #Create dictionaries with movie info
 inception = {
     "name": "Inception",
@@ -30,18 +32,39 @@ def addFilm(list, name, year, rating):
             "rating": rating
         }
 
-        list.append[newDictionary]
+        list.append(newDictionary)
 
-    #Check if user wants to add another film    
-    uInput = input.lower(("Would you like to add another film? y/n"))
-    if uInput == "n":
-        running = False
-        print(filmCol)
+        #Check if user wants to add another film    
+        uInput = input("Would you like to add another film? y/n")
+        if uInput == "n":
+            running = False
 
-#Save user input in variables for the function
-nameIn = input("What movie would you like to add?\n")
-nameIn.lower()
-yearIn = int(input("What year was it released?\n"))
-ratingIn = float(input("What was it's ratings?\n"))
+#Function to print the dictionary content out in a prettier way
+def showFilmsInList(dicList):
+    print("==========================================")
+    for key in dicList:
+        mName = key.get("name")
+        mYear = key.get("year")
+        mRating = key.get("rating")
+        print(f"{mName} - {mYear} has a rating of {mRating}")
+        print("==========================================")
 
-addFilm(filmCol, nameIn, yearIn, ratingIn)
+#Runns program in a while loop as long as the user wants to
+while programRunning:
+    userAction = int(input("Enter '1' to add movie.'2' to print movies. '3' to exit: "))
+    if userAction == 1:
+        #Save user input in variables for the function
+        nameIn = input("What movie would you like to add?\n")
+        nameIn.lower()
+        yearIn = int(input("What year was it released?\n"))
+        ratingIn = input("What was it's ratings?\n")
+
+        if ratingIn == "":
+            ratingIn = 5.0
+        
+        addFilm(filmCol, nameIn, yearIn, ratingIn)
+
+    elif userAction == 2:
+        showFilmsInList(filmCol)
+    else:
+        programRunning = False
